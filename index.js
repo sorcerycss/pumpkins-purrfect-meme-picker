eeae2576833fe5788809fe08b3eee01c08d8f747
sorcerycss <sorcerycss@gmail.com>
2025-06-04 17:22:41 -0700

    Updated logic

--- index.js
+++ index.js
@@ -169,12 +169,33 @@ const catsData = [
     },
 ]
 
+// function getEmotionsArray(cats) {
+//     const emotionsArray = []
+//     for (let i = 0; i < cats.length; i++) {
+//         for (let j = 0; j < cats[i].emotionTags.length; j++) {
+//             emotionsArray.push(cats[i].emotionTags[j])
+//         }
+//     }
+//     console.log(emotionsArray)
+// }
+
 function getEmotionsArray(cats) {
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++) {
-        for (let j = 0; j < cats[i].emotionTags.length; j++) {
-            emotionsArray.push(cats[i].emotionTags[j])
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            emotionsArray.push(emotion)
         }
     }
-    console.log(emotionsArray)
+    return emotionsArray
 }
+
+function renderEmotionsRadios(cats) {
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions) {
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
