37592ec44ea3c20527d2adcc232aa8e60365b0a5
sorcerycss <sorcerycss@gmail.com>
2025-06-05 16:30:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { catsData } from "./data"
+import { catsData } from './data.js';
 
 // function getEmotionsArray(cats) {
 //     const emotionsArray = []
@@ -14,7 +14,9 @@ function getEmotionsArray(cats) {
     const emotionsArray = []
     for (let cat of cats) {
         for (let emotion of cat.emotionTags) {
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
@@ -24,7 +26,15 @@ function renderEmotionsRadios(cats) {
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions) {
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `<div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions"
+                >
+        </div>`
     }
     emotionRadios.innerHTML = radioItems
 }
