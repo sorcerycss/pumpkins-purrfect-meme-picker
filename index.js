0a2759aa58a7d464f118fa0c02f18238f6dbaaf9
sorcerycss <sorcerycss@gmail.com>
2025-06-08 10:26:32 -0700

    Updated logic and order

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import { catsData } from './data.js';
 
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
@@ -28,8 +31,34 @@ function getMatchingCatsArray() {
                 return cat.emotionTags.includes(selectedEmotion)
             }
         })
-        console.log(matchingCatsArray)
+        return matchingCatsArray
     }
+}
+
+function getSingleCatObject() {
+
+    const catsArray = getMatchingCatsArray()
+
+    if (catsArray.length === 1) {
+        return catsArray[0]
+    }
+    else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+
+}
+
+function renderCat() {
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
 }
 
 function getEmotionsArray(cats) {
