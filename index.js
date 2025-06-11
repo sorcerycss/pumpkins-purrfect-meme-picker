b8351a8a81b1e5dfd790b14db019d1ea30b12366
sorcerycss <sorcerycss@gmail.com>
2025-06-11 09:37:47 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
 const memeModalInner = document.getElementById('meme-modal-inner')
 const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 getImageBtn.addEventListener('click', renderCat)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+function closeModal() {
+    memeModal.style.display = 'none'
+}
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
@@ -54,7 +61,7 @@ function renderCat() {
     memeModalInner.innerHTML = `
         <img 
         class="cat-img" 
-        src="./images/${catObject.image}"
+        src="./img/${catObject.image}"
         alt="${catObject.alt}"
         >
         `
